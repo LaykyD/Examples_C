c01fee3197690a4b724d110280cdae4bd1766568
MosunovDA <layky-dima@mail.ru>
2023-01-12 21:04:03 +0300

    add Example005

---
+++ Example005_CondionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter name user: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Wow, this is Maria!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
